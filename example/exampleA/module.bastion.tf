c2de169ab45aed50f3b3745cbbb36b9b3c49bc3c
Woolfensen, James <jw250550@ncr.com>
2019-08-28 16:51:38 +0100

    fix module

--- example/exampleA/module.bastion.tf
+++ example/exampleA/module.bastion.tf
@@ -1,3 +1,8 @@
 module "bastion" {
     source="../../"
+    image             = var.image
+    project           = var.project    
+    subnetwork_project= var.subnetwork_project
+    subnetwork        = var.subnetwork
+    zone              = var.zone
 }
