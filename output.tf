0c81467428e616f3a38ee7d7cd9defd4ced89d7a
James Woolfenden <james.woolfenden@gmail.com>
2019-11-01 15:56:15 +0000

    revised

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output bastion_node_ip {
+output "bastion_node_ip" {
   value = google_compute_instance.bastion.network_interface.0.network_ip
 }
 
 
-output external_ip {
+output "external_ip" {
   value = google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,64 +1,64 @@
-variable name {
+variable "name" {
   type        = string
   description = "The name of the Bastion Instance"
   default     = "bastion"
 }
 
-variable project {
+variable "project" {
   type        = string
   description = "The GCP project"
 }
 
-variable network_interface {
+variable "network_interface" {
   type        = map
   description = ""
 }
 
-variable zone {
+variable "zone" {
   type        = string
   description = "The GCP zone"
 }
 
-variable machine_type {
+variable "machine_type" {
   type        = string
   description = "The machine type for the Bastion"
   default     = "n1-standard-1"
 }
 
-variable tags {
+variable "tags" {
   description = "Hardcoded tags that associates the correct firewall to the instance"
   type        = "list"
   default     = ["bastion-ssh"]
 }
 
-variable image {
+variable "image" {
   type        = map
   description = "Describes the base image used"
 }
 
-variable source_cidrs {
+variable "source_cidrs" {
   type        = list
   description = "The ranges to allow to connect to the bastion"
 }
 
-variable firewall {
+variable "firewall" {
   description = "Flag to control the creation or not of a firewall rule. Maybe not needed if use a pre-prepared or shared set-up"
   type        = number
   default     = 0
 }
 
-variable service_email {
+variable "service_email" {
   description = "Service account username"
   type        = string
 }
 
-variable service_scope {
+variable "service_scope" {
   type = list
   default = [
   "https://www.googleapis.com/auth/cloud-platform"]
 }
 
-variable nat_ip {
+variable "nat_ip" {
   description = "Values set if using a Static IP"
   default     = null
 }
