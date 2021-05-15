5d0952ccddbb03896f2c7aea060c7e4b8550136f
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-15 12:01:54 +0100

    docd

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     google = {
-      version = "3.57.0"
+      version = "3.59.0"
       source  = "hashicorp/google"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">=0.14.8"
 }
