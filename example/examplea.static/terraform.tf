46a4bad67aa8370aac3878dbf2f24a3ceb538278
James Woolfenden <james.woolfenden@gmail.com>
2026-05-06 12:18:43 +0100

    refresh

--- example/examplea.static/terraform.tf
+++ example/examplea.static/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.30.0"
+      version = "7.31.0"
     }
   }
   required_version = ">= 1.3.5"
